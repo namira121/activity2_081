bb6bcd1db0fa536a1aac128c04f79dc2e22a57f0
namira121 <127211008+namira121@users.noreply.github.com>
2023-03-08 12:07:16 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){//begin
+
+    //Numeric Luas, P, L 
+    double Luas, P, L;
+    //Display "Masukkan Panjangnya = "
+    cout << "Masukkan Panjanganya = ";
+    //Accept P
+    cin >> P;
+    //Display "Masukkan Lebarnya = "
+    cout << "Masukkan Lebarnya = ";
+    //Accept L
+    cin >> L;
+    //Compute Luas = P*L
+    Luas = P*L;
+    //Display "Luasnya = " + Luas
+    cout << "Luasnya = " << Luas;
+}//End
